0c2c34ac91e823c6e83102a2d9b96bef49cfc26e
clark4de-git <clark4de@mail.uc.edu>
2025-04-30 15:33:02 -0400

    Created some functions for checking due date, adding task, and listing tasks (which is the most complicated one but I believe that it works)

--- my_script.py
+++ my_script.py
@@ -21,6 +21,56 @@ def init_db():
     conn.commit()
     conn.close()
 
+# This function checks if the due date argument is formatted right
+def validate_due_date(date_str):
+    if date_str and not re.match('^\d{2}-\d{2}-\d{4}$', date_str):
+        print("Error: Due Date must be in DD-MM-YYYY format.")
+        sys.exit(1)
+
+# This function is for adding tasks to the todolist which requires a
+# task and a due date to be input
+def add_task(task, due_date):
+    validate_due_date(due_date)
+    conn = sqlite3.connect(DB_FILE)
+    courier = conn.cursor()
+    courier.execute('INSERT INTO todolist (task, due_date) VALUES (?, ?)', (task, due_date))
+    conn.commit()
+    conn.close()
+    print("Task added successfully.")
+
+# This function is for listing out the tasks on the todolist
+def list_tasks(due=None, completed=None):
+    conn = sqlite3.connect(DB_FILE)
+    # This query variable is initialized here so that it can be
+    # appended if additional options/args are given in addition to list
+    query = 'SELECT * from todolist WHERE 1=1'
+    # Variable for appending query
+    params = []
+
+    # Optional option due as in due date
+    if due:
+        validate_due_date(due)
+        query += ' AND due_date = ?'
+        params.append(due)
+    
+    # Optional option completed
+    if completed is not None:
+        query += ' AND completed = ?'
+        params.append(int(completed))
+    
+    # Carries out the execution of the query with or without the optional options
+    courier = conn.cursor()
+    courier.execute(query, params)
+    rows = courier.fetchall()
+
+    # Checks for nothing returned and prints out what is returned based on below format
+    if not rows:
+        print("No tasks found.")
+    for row in rows:
+        status = "Completed" if row[3] else " "
+        print(f"{row[0]}, [{status}] {row[1]} | Due: {row[2] or 'N/A'} | Created: {row[4]}")
+    conn.close()
+
 
 
 def main():
